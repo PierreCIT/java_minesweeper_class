074d6992e3caad4b66448d6981ffc5b6e563ab69
Noeim <noeim.jeux@gmail.com>
2019-09-29 18:30:05 +0200

    Added JavaDoc for SortByScore class's methods

--- src/emse/ismin/demineur/SortByScore.java
+++ src/emse/ismin/demineur/SortByScore.java
@@ -6,6 +6,12 @@ import java.util.Comparator;
  * Class that will enable to sort the players by descending score.
  */
 public class SortByScore implements Comparator<Player> {
+    /**
+     * Will compare two player scores in a way that will create descending order when using it to sort a player list
+     * @param a Player a
+     * @param b Player b
+     * @return The difference between player a's score and b's.
+     */
     public int compare(Player a, Player b){
         return b.getScore() - a.getScore();
     }
